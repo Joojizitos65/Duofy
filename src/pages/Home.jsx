af7423fdd1b779495102f7d903742b6661a93525
scrowYko <dmorelpintos@gmail.com>
2024-05-01 18:29:40 -0300

    dia 0105 hora 1829 davi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,69 @@
-import NavBarHome from "../components/navBarHome"
-import '../styles/Home.css'
+import NavBarHome from "../components/navBarHome";
+import "../styles/Home.css";
 
-function Compras(){
-    return(
-        <>
-        <NavBarHome />
-        </>
-    )
+function Home() {
+  return (
+    <>
+      <NavBarHome />
+      <div className="container-ofertas">
+        <div className="images">
+          <img src="src\assets\mequi.jpg" />
+        </div>
+      </div>
+      <section className="home-content">
+        <h1>Os favoritos da galera!</h1>
+        <div className="container-carrosel">
+          <img src="src\assets\mequi.jpg" />
+          <img src="src\assets\mequi.jpg" />
+          <img src="src\assets\mequi.jpg" />
+          <img src="src\assets\mequi.jpg" />
+          <img src="src\assets\mequi.jpg" />
+          <img src="src\assets\mequi.jpg" />
+          <img src="src\assets\mequi.jpg" />
+        </div>
+        <div className="favoritos">
+          <h2>Seus estabelecimentos favoritos!!</h2>
+          <div className="container-favoritos">
+            <img src="src\assets\mequi.jpg" />
+            <img src="src\assets\mequi.jpg" />
+          </div>
+        </div>
+        <div className="fy">
+          <div className="container-produtos">
+            <div className="produto">
+              <img src="src\assets\mequi.jpg" />
+              <p className="name-produto">whopper</p>
+              <p className="price-product">R$ 29,90</p>
+            </div>
+            <div className="produto">
+              <img src="src\assets\mequi.jpg" />
+              <p className="name-produto">whopper</p>
+              <p className="price-product">R$ 29,90</p>
+            </div>
+            <div className="produto">
+              <img src="src\assets\mequi.jpg" />
+              <p className="name-produto">whopper</p>
+              <p className="price-product">R$ 29,90</p>
+            </div>
+            <div className="produto">
+              <img src="src\assets\mequi.jpg" />
+              <p className="name-produto">whopper</p>
+              <p className="price-product">R$ 29,90</p>
+            </div>
+            <div className="produto">
+              <img src="src\assets\mequi.jpg" />
+              <p className="name-produto">whopper</p>
+              <p className="price-product">R$ 29,90</p>
+            </div>
+            <div className="produto">
+              <img src="src\assets\mequi.jpg" />
+              <p className="name-produto">whopper</p>
+              <p className="price-product">R$ 29,90</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
-export default Compras
+export default Home;
