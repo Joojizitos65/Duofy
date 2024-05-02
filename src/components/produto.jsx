437ba7a5215ff1abd9355efd147e083324ae25b8
scrowYko <dmorelpintos@gmail.com>
2024-05-01 23:00:49 -0300

    atualizacao 0105

---
+++ src/components/produto.jsx
@@ -0,0 +1,14 @@
+function Produto() {
+
+    return (
+        <div className="produto">
+        <img src="src\assets\produtos\whopper.jpg" />
+            <div className="info">
+                <p className="name-produto">Whopper</p>
+                <p className="price-product">R$ 29,90</p>
+            </div>
+        </div>
+    )
+}
+
+export default Produto
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,27 @@
 import NavBarHome from "../components/navBarHome";
 import "../styles/Home.css";
+import Produto from "../components/produto";
 
 function Home() {
   return (
     <>
       <NavBarHome />
+      <div className="container-endereco">
+        <select name="endereco" id="endereco-selecionar">
+          <option value="0">
+            <p>Seu endereço</p>
+          </option>
+          <option value="1">
+            <p>Rua Ayrton Senna, 442</p>
+          </option>
+          <option value="2">
+            <p>Rua Elizeu de Bernardes, 576</p>
+          </option>
+        </select>
+      </div>
       <div className="container-ofertas">
         <div className="images">
-          <img src="src\assets\mequi.jpg" />
+          <img src="src\assets\carrosselOfertas\2.png" />
         </div>
       </div>
       <section className="home-content">
@@ -24,42 +38,19 @@ function Home() {
         <div className="favoritos">
           <h2>Seus estabelecimentos favoritos!!</h2>
           <div className="container-favoritos">
-            <img src="src\assets\mequi.jpg" />
-            <img src="src\assets\mequi.jpg" />
+            <img src="src\assets\carrosselOfertas\2.png" />
+            <img src="src\assets\carrosselOfertas\2.png" />
           </div>
         </div>
         <div className="fy">
+          <h1>Escolhidos para você</h1>
           <div className="container-produtos">
-            <div className="produto">
-              <img src="src\assets\mequi.jpg" />
-              <p className="name-produto">whopper</p>
-              <p className="price-product">R$ 29,90</p>
-            </div>
-            <div className="produto">
-              <img src="src\assets\mequi.jpg" />
-              <p className="name-produto">whopper</p>
-              <p className="price-product">R$ 29,90</p>
-            </div>
-            <div className="produto">
-              <img src="src\assets\mequi.jpg" />
-              <p className="name-produto">whopper</p>
-              <p className="price-product">R$ 29,90</p>
-            </div>
-            <div className="produto">
-              <img src="src\assets\mequi.jpg" />
-              <p className="name-produto">whopper</p>
-              <p className="price-product">R$ 29,90</p>
-            </div>
-            <div className="produto">
-              <img src="src\assets\mequi.jpg" />
-              <p className="name-produto">whopper</p>
-              <p className="price-product">R$ 29,90</p>
-            </div>
-            <div className="produto">
-              <img src="src\assets\mequi.jpg" />
-              <p className="name-produto">whopper</p>
-              <p className="price-product">R$ 29,90</p>
-            </div>
+            <Produto />
+            <Produto />
+            <Produto />
+            <Produto />
+            <Produto />
+            <Produto />
           </div>
         </div>
       </section>
