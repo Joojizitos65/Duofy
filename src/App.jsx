1d375d62630fbd121f4ad98f8c39c870152a47a7
scrowYko <dmorelpintos@gmail.com>
2024-05-12 15:26:08 -0300

    arrumando margin na direita da tela

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <>
     <Router>
       <Routes>
-      <Route path='/home' element={<Home/>} />
+      <Route path='/' element={<Home/>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/perfil' element={<Profile/>} />
       <Route path='/conversa' element={<Conversa/>} />
